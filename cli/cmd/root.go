c0fd5d37e34c777fe52b6fa33c2a8bda789b165d
Benjamin Quinn <benjamin.quinn92@gmail.com>
2022-11-07 15:59:04 -0500

    Move subcommand logic into separate file

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -23,20 +23,10 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 
 	"github.com/aurc/loggo/cli/loggo"
 	"github.com/spf13/cobra"
-)
-
-const (
-	cmdPrefix    = "loggo-"
-	cmdGcpStream = "gcp-stream"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -60,65 +50,6 @@ func Initiate() {
 	}
 }
 
-func getExecutablePath() string {
-	ex, err := os.Executable()
-	if err != nil {
-		panic(err)
-	}
-	return filepath.Dir(ex)
-}
-
-func getExecutableSubCommands() []string {
-
-	files, err := ioutil.ReadDir(getExecutablePath())
-	if err != nil {
-		panic(err)
-	}
-
-	subcommands := make([]string, 0)
-	for _, file := range files {
-		if !file.IsDir() && strings.HasPrefix(file.Name(), cmdPrefix) {
-			subcommands = append(subcommands, strings.TrimPrefix(file.Name(), cmdPrefix))
-		}
-	}
-
-	return subcommands
-}
-
-func runExecutableSubcommand(cmdName string) func(cmd *cobra.Command, args []string) {
-	return func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Executing subcommand %s\n", cmdName)
-		p := filepath.Join(getExecutablePath(), cmdPrefix+cmdName)
-		execCmd := exec.CommandContext(cmd.Context(), p, args...)
-		execCmd.Stdout = os.Stdout
-		execCmd.Stderr = os.Stderr
-		execCmd.Stdin = os.Stdin
-		if err := execCmd.Run(); err != nil {
-			fmt.Printf("Failed to run subcommand: %s\n", err)
-			os.Exit(3)
-		}
-
-		os.Exit(execCmd.ProcessState.ExitCode())
-	}
-}
-
-var executableCommands = map[string]*cobra.Command{
-	cmdGcpStream: {
-		Use:   cmdGcpStream,
-		Short: "Continuously stream GCP stack driver logs",
-		Long: `Continuously stream Google Cloud Platform log entries
-from a given selected project and GCP logging filters:
-
-	loggo gcp-stream \ 
-            --project myGCPProject123 \
-            --from 1m \
-            --filter 'resource.labels.namespace_name="awesome-sit" AND resource.labels.container_name="some"' 
-`,
-		DisableFlagParsing: true,
-		Run:                runExecutableSubcommand(cmdGcpStream),
-	},
-}
-
 func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
@@ -129,13 +60,4 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
-	// Add any found exectuable subcommands to root cmd.
-	for _, c := range getExecutableSubCommands() {
-		if cmd, f := executableCommands[c]; f {
-			rootCmd.AddCommand(cmd)
-		} else {
-			panic(fmt.Sprintf("No implementation found for command %s", c))
-		}
-	}
 }
---
+++ cli/cmd/subcmd.go
@@ -0,0 +1,87 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	cmdPrefix    = "loggo-"
+	cmdGcpStream = "gcp-stream"
+)
+
+func getExecutablePath() string {
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Dir(ex)
+}
+
+func getExecutableSubCommands() []string {
+
+	files, err := ioutil.ReadDir(getExecutablePath())
+	if err != nil {
+		panic(err)
+	}
+
+	subcommands := make([]string, 0)
+	for _, file := range files {
+		if !file.IsDir() && strings.HasPrefix(file.Name(), cmdPrefix) {
+			subcommands = append(subcommands, strings.TrimPrefix(file.Name(), cmdPrefix))
+		}
+	}
+
+	return subcommands
+}
+
+func runExecutableSubcommand(cmdName string) func(cmd *cobra.Command, args []string) {
+	return func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Executing subcommand %s\n", cmdName)
+		p := filepath.Join(getExecutablePath(), cmdPrefix+cmdName)
+		execCmd := exec.CommandContext(cmd.Context(), p, args...)
+		execCmd.Stdout = os.Stdout
+		execCmd.Stderr = os.Stderr
+		execCmd.Stdin = os.Stdin
+		if err := execCmd.Run(); err != nil {
+			fmt.Printf("Failed to run subcommand: %s\n", err)
+			os.Exit(3)
+		}
+
+		os.Exit(execCmd.ProcessState.ExitCode())
+	}
+}
+
+var executableCommands = map[string]*cobra.Command{
+	cmdGcpStream: {
+		Use:   cmdGcpStream,
+		Short: "Continuously stream GCP stack driver logs",
+		Long: `Continuously stream Google Cloud Platform log entries
+from a given selected project and GCP logging filters:
+
+	loggo gcp-stream \ 
+            --project myGCPProject123 \
+            --from 1m \
+            --filter 'resource.labels.namespace_name="awesome-sit" AND resource.labels.container_name="some"' 
+`,
+		DisableFlagParsing: true,
+		Run:                runExecutableSubcommand(cmdGcpStream),
+	},
+}
+
+func init() {
+	// Add any found exectuable subcommands to root cmd.
+	for _, c := range getExecutableSubCommands() {
+		if cmd, f := executableCommands[c]; f {
+			rootCmd.AddCommand(cmd)
+		} else {
+			panic(fmt.Sprintf("No implementation found for command %s", c))
+		}
+	}
+}
